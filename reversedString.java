6f8104452da677ac493bdd0159fee6ed635b48eb
SANV <129673601+vSan-V@users.noreply.github.com>
2024-12-06 07:41:45 +0530

    Create reversedString.java

---
+++ reversedString.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String args[]) {
+		String name="Sanjai";
+		char[] store = new char[name.length()];
+		for(int i = name.length()-1; i>=0; i--) {
+			store[i]=name.charAt(name.length()-i-1);
+		}
+		String reversed = new String(store);
+		System.out.print(store);
+	}
+}
