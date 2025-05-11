19e5ccab4458f1e6b67b9ccf438c9cd690a65197
Sanjai <129673601+vSan-V@users.noreply.github.com>
2025-05-11 06:18:10 +0530

    Add files via upload

---
+++ leap_year.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        int year=2005;
+        
+        if((year%4==0 && year%100 !=0) || (year%400==0)){
+            System.out.println("leap year");
+        }
+        else{
+            System.out.println("not a leap year");
+        }
+    }
+}
